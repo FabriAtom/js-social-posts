9124b316f1f1c2c82df0c7b05d76566935330828
FabriAtom <fabrizioromano20@gmail.com>
2022-08-06 13:22:23 +0200

    main

---
+++ main.js
@@ -0,0 +1,91 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+// RECUPERO CONTAINER
+// CREO ARRAY VUOTO PER LIKES
+
+// CICLO FOREACH POSTS
+    // CREO VAR IMAGE  = ELEMENT.AUTHOR.IMAGE
+    // AGGIUNGI GUARDONE AL BOTTONE
+    // INCREMENTARE NUM LIKES
+            // idpost = tjis.dataset.postid
+            // const postElement = queryselector('#post-' + idpost + '.js-like-counter')
+            // postElement.innerhtml++
+
+        // CAMBIARE COLORE DEL TESTO BOTTONE
+        this.classlist.add('active')
+        this.innerhtml = 'you like this'
+    
+    // CREO VAR BTN-LIKEHELL = TAMPLATE LIT DEL PULSANTE
+    // SE !IMAGE
+        // INSERICO IMMAGINE PROVVISORIA
+
+    // CONTAINER INNERHTML TEMPLATE LIT DEL POST 
+
+
+
+
+
+
+
+
+    // const likeButton =
+    //            <a class="like-button  js-like-button" href="#" data-postid="1">
+    //               <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+    //               <span class="like-button__label">Mi Piace</span>
+    //           </a>
