c662fcdaabc6629de93d3616ae2b0b8aec16134d
FabriAtom <fabrizioromano20@gmail.com>
2022-08-24 16:30:31 +0200

    main.js

--- main.js
+++ main.js
@@ -56,6 +56,69 @@ const posts = [
     }
 ];
 
+const containerEl = document.getElementById('container')
+containerEl.innerHTML = ''
+
+for (let i = 0; < posts.length; i++ ) {
+    const post = posts[i]
+    console.log(post)
+
+    const html =
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post.author.name}</div>
+                        <div class="post-meta__time">${post.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${post.content}</div>
+            <div class="post__image">
+                <img src="${post.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    </div>
+
+
+
+    containerEl.innerHTML += html
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // RECUPERO CONTAINER
 // CREO ARRAY VUOTO PER LIKES
 
